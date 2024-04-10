a9321657d36a05c2078d66c80e95ee7f522bb119
linka <ilincnedel2710@student.lvusd.org>
2024-04-09 20:29:22 -0700

    change and fix keypad+some of the leds

--- keypad/keypad.ino
+++ keypad/keypad.ino
@@ -1,7 +1,7 @@
 #include <Key.h>
 #include <Keypad.h>
 
-const byte LED1_RED = 9;
+const byte LED1_RED = 13;
 const byte LED1_BLUE = 12;
 const byte LED2_RED = 11;
 const byte LED2_BLUE = 10;
@@ -9,33 +9,33 @@ const byte LED3_RED = 54;
 const byte LED3_BLUE = 55;
 const byte LED4_RED = 56;
 const byte LED4_BLUE = 57;
-const byte LED5_RED = 58;
-const byte LED5_BLUE = 59;
+const byte LED5_RED = 59;
+const byte LED5_BLUE = 58;
 
-const byte LED6_RED = 60;
-const byte LED6_BLUE = 61;
-const byte LED7_RED = 68;
-const byte LED7_BLUE = 69;
+const byte LED6_RED = 61;
+const byte LED6_BLUE = 60;
+const byte LED7_RED = 67;
+const byte LED7_BLUE = 68;
 const byte LED8_RED = 62;
 const byte LED8_BLUE = 63;
 const byte LED9_RED = 64;
 const byte LED9_BLUE = 65;
-const byte LED10_RED = 66;
-const byte LED10_BLUE = 67;
+const byte LED10_RED = 69;
+const byte LED10_BLUE = 66;
 
 
-const byte ROWS = 4; //four rows
-const byte COLS = 4; //three columns
+const byte ROWS = 4; 
+const byte COLS = 4; 
 char keys[ROWS][COLS] =
 {
-  {'1', '2', '3','A'},
+  {'1', '2', '3', 'A'},
   {'4', '5', '6', 'B'},
   {'7', '8', '9', 'C'},
-  {'*', '0', '#','D'}
+  {'*', '0', '#', 'D'}
 };
 
-byte rowPins[ROWS] = { 7, 6, 5, 4}; //connect to the row pinouts of the keypad
-byte colPins[COLS] = { 3, 2, 1, 0}; //connect to the column pinouts of the keypad
+byte rowPins[ROWS] = {9, 8, 7, 6}; //connect to the row pinouts of the keypad
+byte colPins[COLS] = {5, 4, 3, 2}; //connect to the column pinouts of the keypad
 
 Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
 
@@ -67,70 +67,67 @@ void setup()
 
 void loop()
 {
-  
   char key = keypad.getKey();
-  //Serial.print("button ");
-  //Serial.print(key);
+  
   switch (key)
   {
     case '1':
-      digitalWrite(LED1_RED, HIGH);
-      digitalWrite(LED1_BLUE, LOW);
+    Serial.print("button 1");
+      digitalWrite(LED1_BLUE, HIGH);
       delay(1000);
       break;
     
     case '2':
-      digitalWrite(LED1_BLUE, HIGH);
-      digitalWrite(LED1_RED, LOW);
+    Serial.print("button 2");
+      digitalWrite(LED2_BLUE, HIGH);
       delay(1000);
       break;
 
     case '3':
-      digitalWrite(LED2_RED, HIGH);
-      digitalWrite(LED3_BLUE, LOW);
+    Serial.print("button 3");
+      digitalWrite(LED3_BLUE, HIGH);
       delay(1000);
       break;
     
     case '4':
-      digitalWrite(LED2_BLUE, HIGH);
-      digitalWrite(LED2_RED, LOW);
+    Serial.print("button 4");
+      digitalWrite(LED4_BLUE, HIGH);
       delay(1000);
       break;
 
     case '5':
-    Serial.print("button 6");
-      digitalWrite(LED3_RED, HIGH);
-      digitalWrite(LED3_BLUE, LOW);
+    Serial.print("button 5");
+      digitalWrite(LED5_BLUE, HIGH);
       delay(1000);
       break;
     
     case '6':
-      digitalWrite(LED3_BLUE, HIGH);
-      digitalWrite(LED3_RED, LOW);
+    Serial.print("button 6");
+      digitalWrite(LED6_BLUE, HIGH);
       delay(1000);
       break;
       
     case '7':
-      digitalWrite(LED4_RED, HIGH);
-      digitalWrite(LED4_BLUE, LOW);
+    Serial.print("button 7");
+      digitalWrite(LED7_BLUE, HIGH);
       delay(1000);
       break;
     
     case '8':
-      digitalWrite(LED4_BLUE, HIGH);
-      digitalWrite(LED4_RED, LOW);
+    Serial.print("button 8");
+      digitalWrite(LED8_BLUE, HIGH);
       delay(1000);
       break;
 
     case '9':
-      digitalWrite(LED5_RED, HIGH);
-      digitalWrite(LED5_BLUE, LOW);
+    Serial.print("button 9");
+      digitalWrite(LED9_BLUE, HIGH);
       delay(1000);
       break;
     
     case '0':
-      digitalWrite(LED5_BLUE, HIGH);
-      digitalWrite(LED5_RED, LOW);
+    Serial.print("button 10");
+     digitalWrite(LED10_BLUE, HIGH);
       delay(1000);
       break;
   } 
